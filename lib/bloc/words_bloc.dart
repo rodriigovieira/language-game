34849cb35cdb685d1c78e0b7b9fc416afed834f8
Rodrigo Vieira <rodriigovieira@gmail.com>
2020-02-18 11:24:16 -0300

    sending last changes

--- lib/bloc/words_bloc.dart
+++ lib/bloc/words_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:language_game/bloc/home_page_bloc.dart';
 import 'package:language_game/constants.dart';
 import 'package:language_game/models/word_model.dart';
+import 'package:language_game/screens/HomeScreen/home_screen.dart';
 
 class WordsBloc extends ChangeNotifier {
   Future<void> handleWordAdd({
@@ -28,7 +30,28 @@ class WordsBloc extends ChangeNotifier {
 
     await insertWord(word);
 
-    Navigator.pop(context);
-    Navigator.pop(context);
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration(milliseconds: 300),
+        pageBuilder: (_, __, ___) => HomeScreen(),
+        transitionsBuilder: (
+          BuildContext context,
+          Animation animation1,
+          Animation animation2,
+          Widget child,
+        ) {
+          final tween = Tween(
+            begin: Offset(0.0, 1.0),
+            end: Offset.zero,
+          );
+
+          return SlideTransition(
+            position: animation1.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
   }
 }
